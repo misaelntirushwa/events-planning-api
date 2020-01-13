b4138fe1f233781153b9023c29fe251e8a1fb295
Misael Ntirushwa <misaelntirushwa@gmail.com>
2020-01-12 22:59:53 -0500

    initial commit

---
+++ src/test/java/com/mnt/eventsplanning/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnt.eventsplanning.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
